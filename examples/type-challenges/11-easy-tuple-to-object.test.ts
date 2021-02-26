73aa796c3c97416628544257a6ca636b0788dc42
mistlog <mistlog@outlook.com>
2021-02-26 09:16:07 +0800

    add  TupleToObject

---
+++ examples/type-challenges/11-easy-tuple-to-object.test.ts
@@ -0,0 +1,21 @@
+/**
+ * ref:
+ * - https://github.com/type-challenges/type-challenges/blob/master/questions/11-easy-tuple-to-object/README.md
+ * - https://github.com/type-challenges/type-challenges/issues/945
+ */
+import { TupleToObject } from "./11-easy-tuple-to-object";
+import { Test } from "ts-toolbelt"
+const { checks, check } = Test
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type result = TupleToObject<typeof tuple>;
+
+checks([
+    check<result, {
+        tesla: "tesla";
+        "model 3": "model 3";
+        "model X": "model X";
+        "model Y": "model Y";
+    }, Test.Pass>(),
+])
---
+++ examples/type-challenges/11-easy-tuple-to-object.ts
@@ -0,0 +1,1 @@
+export type TupleToObject<T extends readonly any[]> = { [K in T[number]]: K };
