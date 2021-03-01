a92d4324942a86d9f90ca8155b004d29139cc9b7
mistlog <mistlog@outlook.com>
2021-03-01 23:55:45 +0800

    refine Chainable

--- examples/type-challenges/12-medium-chainable-options.ts
+++ examples/type-challenges/12-medium-chainable-options.ts
@@ -2,5 +2,4 @@ export type Chainable<Props = {}> = {
   option: OptionType<Props>;
   get: () => Props;
 };
-type OptionType<Props = {}> = <Key extends string, Value>(key: Key, value: Value) => Chainable<Props & toObjectType<Key, Value>>;
-type toObjectType<Keys extends string, value> = { [key in Keys]: value };
+type OptionType<Props = {}> = <Key extends string, Value>(key: Key, value: Value) => Chainable<Props & Record<Key, Value>>;
