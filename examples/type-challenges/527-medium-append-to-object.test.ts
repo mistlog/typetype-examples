9b53493b0ac16271cc4cf4db54fc6d6291db148d
mistlog <mistlog@outlook.com>
2021-03-06 08:18:55 +0800

    add AppendToObject example

---
+++ examples/type-challenges/527-medium-append-to-object.test.ts
@@ -0,0 +1,52 @@
+/**
+ * ref:
+ * - https://github.com/type-challenges/type-challenges/blob/master/questions/527-medium-append-to-object/README.md
+ * - https://github.com/type-challenges/type-challenges/issues/766
+ */
+
+import { Test } from "ts-toolbelt"
+import { AppendToObject } from "./527-medium-append-to-object"
+const { checks, check } = Test
+
+type test1 = {
+    key: 'cat'
+    value: 'green'
+}
+
+type testExpect1 = {
+    key: 'cat'
+    value: 'green'
+    home: boolean
+}
+
+type test2 = {
+    key: 'dog' | undefined
+    value: 'white'
+    sun: true
+}
+
+type testExpect2 = {
+    key: 'dog' | undefined
+    value: 'white'
+    sun: true
+    home: 1
+}
+
+type test3 = {
+    key: 'cow'
+    value: 'yellow'
+    sun: false
+}
+
+type testExpect3 = {
+    key: 'cow'
+    value: 'yellow'
+    sun: false
+    isMotherRussia: false | undefined
+}
+
+checks([
+    check<AppendToObject<test1, 'home', boolean>, testExpect1, Test.Pass>(),
+    check<AppendToObject<test2, 'home', 1>, testExpect2, Test.Pass>(),
+    check<AppendToObject<test3, 'isMotherRussia', false | undefined>, testExpect3, Test.Pass>(),
+])
---
+++ examples/type-challenges/527-medium-append-to-object.ts
@@ -0,0 +1,1 @@
+export type AppendToObject<T, Key extends PropertyKey, Value> = { [key in keyof T | Key]: key extends keyof T ? T[key] : Value };
