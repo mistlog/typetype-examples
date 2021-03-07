f1124f3721b3c21e57081bea654960a402f01039
mistlog <mistlog@outlook.com>
2021-03-07 10:49:41 +0800

    feature: use debug option

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { readdirSync, readFileSync, writeFileSync } from "fs";
 import { resolve, basename } from "path";
 import { transform } from "@mistlog/typetype";
+import { exit } from "process";
 
 const exampleDirs = readdirSync("./examples")
     .map(name => resolve(__dirname, "./examples/", name));
@@ -12,15 +13,18 @@ exampleDirs.forEach(dir => {
 
     examples.forEach(example => {
         const source = readFileSync(example, "utf8");
-        
+
         const name = basename(example).replace(".type", "");
         try {
-            const code = transform(source).code;
+            const code = transform(source, { debug: true }).code;
             writeFileSync(resolve(dir, `${name}.ts`), code, "utf8");
-            console.log(`generate ts type file: ${name}.ts`);
         } catch (error) {
             console.log(`failed: ${name}.ts`);
-            console.log(error);
+            console.log(error.message);
+            console.log(error.backtrace);
+            exit(-1)
         }
     })
 })
+
+console.log(`generate ts type file succeed`);
