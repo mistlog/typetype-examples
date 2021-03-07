f9b0b14e12436636fb4579d728a17159c226ec36
mistlog <mistlog@outlook.com>
2021-03-07 17:03:17 +0800

    refine SimpleVue example

--- examples/type-challenges/6-hard-simple-vue.ts
+++ examples/type-challenges/6-hard-simple-vue.ts
@@ -1,5 +1,6 @@
 export type OptionType<dataType, methodType, computedType> = {
   data?: () => dataType;
   computed?: (computedType & ThisType<(dataType & methodType)>);
-  methods?: (methodType & ThisType<(dataType & methodType & { [key in keyof computedType]: computedType[key] extends (...args: any[]) => any ? ReturnType<computedType[key]> : never })>);
+  methods?: (methodType & ThisType<(dataType & methodType & ProcessedComputedType<computedType>)>);
 };
+type ProcessedComputedType<T> = { [key in keyof T]: T[key] extends (...args: any[]) => infer returnType ? returnType : never };
