9e3479561816df9da330a8f94ac1c7d1ba6487ae
mistlog <mistlog@outlook.com>
2021-03-09 08:26:20 +0800

    add isRequired docs

--- examples/type-challenges/57-hard-get-required.test.ts
+++ examples/type-challenges/57-hard-get-required.test.ts
@@ -13,3 +13,16 @@ checks([
     check<GetRequired<{ foo: number, bar?: string }>, { foo: number }, Test.Pass>(),
     check<GetRequired<{ foo: undefined, bar?: undefined }>, { foo: undefined }, Test.Pass>(),
 ])
+
+{
+    type obj = { foo: number, bar?: string }
+
+    type foo = Pick<obj, "foo">; // { foo: number }
+    type bar = Pick<obj, "bar">; // { bar?: string | undefined }
+
+    type foo2 = Record<"foo", obj["foo"]>; // { foo: number }
+    type bar2 = Record<"bar", obj["bar"]>; // { bar: string | undefined }
+
+    type result1 = foo extends foo2 ? true : false; // true
+    type result2 = bar extends bar2 ? true : false; // false
+}
