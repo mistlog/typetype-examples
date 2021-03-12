b741b5b6c11c6c0cecf6c2b7953425f9204b5e8a
mistlog <mistlog@outlook.com>
2021-03-12 22:19:13 +0800

    refine VueBasicProps

--- examples/type-challenges/213-hard-vue-basic-props.ts
+++ examples/type-challenges/213-hard-vue-basic-props.ts
@@ -4,9 +4,9 @@ export type OptionType<dataType, methodType, computedType, propsType> = {
   methods?: (methodType & ThisType<(dataType & methodType & transformComputed<computedType> & transformProps<propsType>)>);
   props?: propsType;
 };
-type transformComputed<computed> = { [key in keyof computed]: computed[key] extends (...args: any) => any ? ReturnType<computed[key]> : never };
+type transformComputed<computed> = { [key in keyof computed]: computed[key] extends (...args: any) => infer returnType ? returnType : never };
 type transformProps<props> = { [key in keyof props]: transformProp<props[key]> };
 type transformProp<prop> = prop extends {
   type: infer propType;
 } ? propType extends Array<infer EachProp> ? transformFunction<EachProp> : transformFunction<propType> : {} extends prop ? any : transformFunction<prop>;
-type transformFunction<T> = T extends (...args: any) => any ? ReturnType<T> : T extends new (...args: any) => any ? InstanceType<T> : never;
+type transformFunction<T> = T extends (...args: any) => infer returnType ? returnType : T extends new (...args: any) => infer instanceType ? instanceType : never;
